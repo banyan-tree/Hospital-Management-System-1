d10a8d36aca6c0d05d5e39141f7b3a57884ce7ca
Sonam17 <scp8613@rit.edu>
2015-04-16 21:37:53 -0400

    test

--- nurse.php
+++ nurse.php
@@ -4,11 +4,11 @@ if (isset($_POST['admission_ID'])) {
     $query.=$_POST['admission_ID'] . "';";
     $result = mysql_query($query);
 
-	if (!$result) {
+    if (!$result) {
         die("Update query failed." . mysql_error() . $query);
-	} else {
-    	header("location: profile.php");
-	}
+    } else {
+        header("location: profile.php");
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -18,63 +18,56 @@ if (isset($_POST['admission_ID'])) {
     <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <b><center>Database Output</center></b><br><br>
-	<?php
-    	////*****NURSE******\\\\\\\\
-	    if (strcmp($login_session_role, "NURSE")  == 0){
-        	$query="SELECT admission_ID, patient_name,";
-        	$query.=" DATE_FORMAT(admission_Date, '%Y-%m-%d'),";
-        	$query.=" DATE_FORMAT(discharge_Date, '%Y-%m-%d'), bed_Number,";
-        	$query.=" doctor FROM test.nurse_patient_discharge,test.login";
-        	$query.=" where test.login.username = '$login_session_user' and";
-        	$query.=" login.unique_ID = nurse_patient_discharge.incharge;";
+<b><center>Database Output</center></b><br><br>
+<?php
+////*****NURSE******\\\\\\\\
+if (strcmp($login_session_role, "NURSE")  == 0){
+    ?>
 
-        	$result = mysql_query($query);
-	        if ($result === FALSE) {
-    	        die("Error getting Nurse info\n" . mysql_error());
-	        }
+	    	<button type="submit" value="Add Patient" onClick="document.location.href='new_patient.php'">Add Patient</button>
+	    	<hr>
+	    	<?php
 
-    	    $num = mysql_numrows($result);
-	        $i = 0;
-    	    while ($i < $num) {
-	            $admission_ID = mysql_result($result,$i,"admission_ID");
-    	        $name = mysql_result($result, $i, "patient_name");
-        	    $admission_Date = mysql_result($result, $i, "DATE_FORMAT(admission_Date, '%Y-%m-%d')");
-            	$date = mysql_result($result, $i, "DATE_FORMAT(discharge_Date, '%Y-%m-%d')");
-	            $bed_Number = mysql_result($result, $i, "bed_Number");
-    	        $doctor = mysql_result($result, $i, "doctor");
-	            ?><p><?php
-	            echo "<b>Admission ID: $admission_ID</b><br>Name: $name<br>Admission_Date: $admission_Date<br>Discharge Date: $date<br>Bed Number: $bed_Number<br>Doctor: $doctor<br>";
+    $query="SELECT admission_ID, patient_name,";
+    $query.=" DATE_FORMAT(admission_Date, '%Y-%m-%d'),";
+    $query.=" DATE_FORMAT(discharge_Date, '%Y-%m-%d'), bed_Number,";
+    $query.=" doctor FROM test.nurse_patient_discharge,test.login";
+    $query.=" where test.login.username = '$login_session_user' and";
+    $query.=" login.unique_ID = nurse_patient_discharge.incharge;";
 
-    	        $datetime1 = new DateTime($date);
-        	    $datetime2 = new DateTime('NOW');
-            	$interval = date_diff($datetime1, $datetime2);
-	
-	            if (strcmp($interval->format('%a'),'0') == 0) {
-    	            ?>
+    $result = mysql_query($query);
+    if ($result === FALSE) {
+        die("Error getting Nurse info\n" . mysql_error());
+    }
+
+    $num = mysql_numrows($result);
+    $i = 0;
+    while ($i < $num) {
+        $admission_ID = mysql_result($result,$i,"admission_ID");
+        $name = mysql_result($result, $i, "patient_name");
+        $admission_Date = mysql_result($result, $i, "DATE_FORMAT(admission_Date, '%Y-%m-%d')");
+        $date = mysql_result($result, $i, "DATE_FORMAT(discharge_Date, '%Y-%m-%d')");
+        $bed_Number = mysql_result($result, $i, "bed_Number");
+        $doctor = mysql_result($result, $i, "doctor");
+        ?><p><?php
+        echo "<b>Admission ID: $admission_ID</b><br>Name: $name<br>Admission_Date: $admission_Date<br>Discharge Date: $date<br>Bed Number: $bed_Number<br>Doctor: $doctor<br>";
+
+        $datetime1 = new DateTime($date);
+        $datetime2 = new DateTime('NOW');
+        $interval = date_diff($datetime1, $datetime2);
+
+        if (strcmp($interval->format('%a'),'0') == 0) {
+            ?>
 					<form method="POST" action="">
 						<input type="HIDDEN" name="admission_ID" value=<?=$admission_ID?> />
 						<button type="submit" name="submit" value="Discharge">Discharge</button>
 					</form>
        				</p><?php
-	            }
-            	?><hr><?php
-	            $i++;
-    	    }
-    	    
-    		?>
-    		
-    		<button type="submit" value="Add Patient" style="float: left; background-color:#FFBC00; color:#fff;
-	        margin-top: 15px;
-            border:2px solid #FFCB00;
-            padding:10px;
-            font-size:20px;
-            cursor:pointer;
-            border-radius:5px;
-            margin-bottom:15px"onClick="document.location.href='new_patient.php'">Add Patient</button>
-            
-            <?php
-    	}
-		?>
+        }
+        ?><hr><?php
+        $i++;
+    }
+}
+?>
 </body>
 </html>
